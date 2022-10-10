d27a2599c885561f1a954027ec5fc725717d0a5f
Jordan Seiler <33209724+JDSeiler@users.noreply.github.com>
2022-10-09 20:00:20 -0400

    Initial project generation with mix

---
+++ lib/ptolemy.ex
@@ -0,0 +1,18 @@
+defmodule Ptolemy do
+  @moduledoc """
+  Documentation for `Ptolemy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ptolemy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
