14f1400149141a46c2bb8004ddc5bbe32cb7ff8b
JDSeiler <jseiler@radford.edu>
2022-12-17 15:31:24 -0500

    Bump elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ptolemy.MixProject do
     [
       app: :ptolemy,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
