699fe45b430462216717bc1f069b261f7a97494a
JDSeiler <jseiler@radford.edu>
2022-12-28 12:05:50 -0500

    Update email link to point to local webpack server, for now

--- lib/ptolemy/controllers/auth/create.ex
+++ lib/ptolemy/controllers/auth/create.ex
@@ -85,7 +85,7 @@ defmodule Ptolemy.Controllers.Auth.Create do
     encoded_query_params = URI.encode_query(query_params)
 
     html_body =
-      "Please verify your email by visiting the following link: <a href=\"http://localhost:4001?#{encoded_query_params}\">Click to verify</a>"
+      "Please verify your email by visiting the following link: <a href=\"http://localhost:3000/verify?#{encoded_query_params}\">Click to verify</a>"
 
     recipients = [
       %{
