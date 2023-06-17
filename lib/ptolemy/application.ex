d1d55418c27bf30d37c1c13f3e7616098f5a597d
JDSeiler <jseiler@radford.edu>
2023-06-17 11:33:37 -0400

    Replace entire app with Phoenix starter template

--- lib/ptolemy/application.ex
+++ lib/ptolemy/application.ex
@@ -7,16 +7,32 @@ defmodule Ptolemy.Application do
 
   @impl true
   def start(_type, _args) do
-    port = 4001
     children = [
+      # Start the Telemetry supervisor
+      PtolemyWeb.Telemetry,
+      # Start the Ecto repository
       Ptolemy.Repo,
-      {Plug.Cowboy, scheme: :http, plug: Ptolemy.RootRouter, options: [port: port]}
+      # Start the PubSub system
+      {Phoenix.PubSub, name: Ptolemy.PubSub},
+      # Start Finch
+      {Finch, name: Ptolemy.Finch},
+      # Start the Endpoint (http/https)
+      PtolemyWeb.Endpoint
+      # Start a worker by calling: Ptolemy.Worker.start_link(arg)
+      # {Ptolemy.Worker, arg}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Ptolemy.Supervisor]
-    IO.puts("Listening on localhost:#{port}")
     Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    PtolemyWeb.Endpoint.config_change(changed, removed)
+    :ok
   end
 end
---
+++ test/ptolemy_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule PtolemyWeb.ErrorJSONTest do
+  use PtolemyWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert PtolemyWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert PtolemyWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
